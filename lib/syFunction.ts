e3993decd4e8a8d13b199ecf78868490bfc2b65e
Victor Peralta <peralta.r.victor@gmail.com>
2021-02-08 19:33:52 -0800

    Add http integration to custom function construct

--- lib/syFunction.ts
+++ lib/syFunction.ts
@@ -1,15 +1,19 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
+import { PayloadFormatVersion } from "@aws-cdk/aws-apigatewayv2";
 
 export interface syFunctionProps {
   srcPath: string;
-  layers: lambda.ILayerVersion[];
+  layers?: lambda.ILayerVersion[];
   functionId: string;
   tableName?: string;
+  env?: Record<string, unknown>;
 }
 
 export class syLambdaFunction extends cdk.Construct {
   public readonly function: lambda.Function;
+  public readonly httpIntegration: LambdaProxyIntegration;
 
   constructor(scope: cdk.Construct, id: string, props: syFunctionProps) {
     super(scope, id);
@@ -20,9 +24,15 @@ export class syLambdaFunction extends cdk.Construct {
       code: lambda.Code.fromAsset(props.srcPath),
       environment: {
         TABLE_NAME: props.tableName ?? "",
+        ...props.env,
       },
       memorySize: 1024,
       layers: props.layers,
+    });
+
+    this.httpIntegration = new LambdaProxyIntegration({
+      handler: this.function,
+      payloadFormatVersion: PayloadFormatVersion.VERSION_2_0,
     });
   }
 }
